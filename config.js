f6731dd82a7778556a4185176f763f523d7fab82
Iga <igcwiklinska@gmail.com>
2024-06-27 10:49:10 +0200

    config package.json update

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const { expect } = require('chai');
-const baseURL = 'https://petstore.swagger.io/';
+// const baseURL = 'https://petstore.swagger.io/';
+const baseURL = 'http://localhost/';
 const request = require('supertest')(baseURL);
 
 module.exports = {
--- test-data/input.data.js
+++ test-data/input.data.js
@@ -1,7 +1,8 @@
 const petIds = {
   toGet: 1719327904428,
-  toGetOrder: 2,
+  toGetOrder: 9,
   toPut: 1719322447757,
+  toDelete: 3,
 };
 
 module.exports = {
