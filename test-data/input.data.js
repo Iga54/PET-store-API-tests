2cd7b65db00e76e6306239e458d0e287c198d736
Iga <igcwiklinska@gmail.com>
2024-07-01 11:26:39 +0200

    update

--- test-data/input.data.js
+++ test-data/input.data.js
@@ -1,9 +1,14 @@
 const petIds = {
   toGetPet: 2,
-  toGetOrder: 9,
+  toGetOrder: 1,
   toPut: 4,
+};
+const userData = {
+  username: 'Tom',
+  password: 'Scott35yh',
 };
 
 module.exports = {
   petIds,
+  userData,
 };
