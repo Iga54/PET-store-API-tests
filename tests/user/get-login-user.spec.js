c001e2de45666907a2d4c5c28d92d5aa91c94cdf
Iga <igcwiklinska@gmail.com>
2024-07-18 12:51:26 +0200

    fixing get login and logout tests - new assertion

--- tests/user/get-login-user.spec.js
+++ tests/user/get-login-user.spec.js
@@ -2,44 +2,29 @@ const { expect, request } = require('../../config');
 const { userData } = require('../../test-data/input.data');
 
 describe('GET user/login', function () {
-  it.only('should login a user of store with params', async function () {
+  it('should login a user of store with params', async function () {
     // Arrange:
     const expectedStatusCode = 200;
     const username = userData.username;
     const password = userData.password;
     // Act:
-    const response = await request.get(
-      `user/login?username=${username}&password=${password}`,
+    let responseText;
+    let responseStatusCode;
 
-    );
-    console.log(response)
+    try {
+      const response = await request.get(
+        `user/login?username=${username}&password=${password}`,
+      );
+    } catch (error) {
+      responseText = error.rawResponse;
+      responseStatusCode = error.statusCode;
+    }
+
     // Assert:
-    expect(response.statusCode).to.be.equal(
+    expect(responseText).to.include('Logged in');
+    expect(responseStatusCode).to.be.equal(
       expectedStatusCode,
       `For GET user/login we expect status code ${expectedStatusCode}`,
     );
   });
 });
-
- // Logowanie surowej odpowiedzi tekstowej
- console.log(response.text);
-
- // Sprawdzanie nagłówków odpowiedzi
- const contentType = response.headers['content-type'];
- let responseBody;
-
- if (contentType && contentType.includes('application/json')) {
-   // Parsowanie odpowiedzi jako JSON
-   responseBody = JSON.parse(response.text);
- } else {
-   // Obsługa odpowiedzi jako tekst
-   responseBody = { message: response.text };
- }
-
- // Sprawdzanie poprawności odpowiedzi
- if (responseBody.message) {
-   expect(responseBody.message).toMatch(/Logged in/);
- } else {
-   expect(responseBody.status).toBe('success');
-   expect(responseBody.message).toBe('Logged in');
- }
--- tests/user/get-user-logout.spec.js
+++ tests/user/get-user-logout.spec.js
@@ -2,16 +2,23 @@ const { expect, request } = require('../../config');
 const { userData } = require('../../test-data/input.data');
 
 describe('GET logout', function () {
-  it('should log out a user of store', async function () {
+  it.only('should log out a user of store', async function () {
     // Arrange:
     const expectedStatusCode = 200;
     // Act:
-    const response = await request.get('user/logout');
+    let responseText;
+    let responseStatusCode;
+    try {
+      const response = await request.get('user/logout')
+    } catch (error) {
+      responseText = error.rawResponse;
+      responseStatusCode = error.statusCode;
+    }
+
     // Assert:
-    expect(response.statusCode).to.be.equal(
+    expect(responseStatusCode).to.be.equal(
       expectedStatusCode,
       `For GET user/logout we expect status code ${expectedStatusCode}`,
-      expect(response.body.message).to.include("ok")
     );
   });
 });
